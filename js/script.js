0d50b3c1cf2dfc22457d1bfcbdc00083f20e7f88
janet-do <124389477+janet-do@users.noreply.github.com>
2023-05-16 01:41:27 +0000

    sync

--- js/script.js
+++ js/script.js
@@ -1,5 +1,27 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
-//
-// Created by: Mr. Coxall
+// Created by: Janet Do
 // Created on: Sep 2020
-// This file contains the JS functions for index.html
+// This function does long division and keeps subtracting until the remainder is smaller than the divisor
+
+"use strict"
+
+function calculate() {
+  // This function divides two numbers
+
+  // input
+  const number1 = parseInt(document.getElementById("number1").value)
+  const number2 = parseInt(document.getElementById("number2").value)
+
+  // process
+  let counter = 0
+  let answer = 0
+  let remainder = number1
+  while (remainder >= number2) {
+    remainder -= number2
+    counter++
+  }
+  answer = counter;
+
+  // output
+  document.getElementById("numberEntered").innerHTML =
+    number1 + " / " + number2 + " = " + answer + " R:" + remainder
+}
